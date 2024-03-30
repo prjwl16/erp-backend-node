6571890e554b139a052c82293d2ae70371f5b23d
prjwl16 <prajumore76@gmail.com>
2024-03-30 14:33:11 +0530

    auto

--- controllers/purchaseOrderTransaction.js
+++ controllers/purchaseOrderTransaction.js
@@ -67,8 +67,76 @@ const createPurchaseOrderTransaction = async (req, res) => {
   }
 }
 
+const deletePurchaseOrderTransaction = async (req, res) => {
+  try {
+    let { purchaseOrderTransactionId } = req.params
+
+    if (!purchaseOrderTransactionId) {
+      return invalidRequest(res, 'Purchase order transaction id is required')
+    }
+
+    purchaseOrderTransactionId = parseInt(purchaseOrderTransactionId)
+
+    const purchaseOrderTransaction = await prisma.purchaseOrderTransaction.findUnique({
+      where: {
+        id: purchaseOrderTransactionId,
+      },
+    })
+
+    if (!purchaseOrderTransaction) {
+      return invalidRequest(res, 'Purchase order transaction not found')
+    }
+
+    const purchaseOrder = await prisma.purchaseOrder.findUnique({
+      where: {
+        id: purchaseOrderTransaction.purchaseOrderId,
+        client: {
+          id: req.user.client.id,
+        },
+      },
+      include: {
+        PurchaseOrderInvoice: true,
+      },
+    })
+
+    if (!purchaseOrder) {
+      return invalidRequest(res, 'Purchase order not found')
+    }
+    console.log(purchaseOrder)
+
+    const txnAmount = parseFloat(purchaseOrderTransaction.amount) || 0
+
+    const totalAmountPaid = parseFloat((purchaseOrder.totalAmountPaid - txnAmount).toFixed(2))
+    const totalAmountDue = parseFloat((purchaseOrder.totalAmountDue + txnAmount).toFixed(2))
+
+    const paymentStatus = totalAmountDue === 0 ? 'PAID' : 'PARTIALLY_PAID'
+
+    const purchaseOrderData = await prisma.purchaseOrder.update({
+      where: {
+        id: purchaseOrderTransaction.purchaseOrderId,
+      },
+      data: {
+        totalAmountPaid: totalAmountPaid,
+        totalAmountDue: totalAmountDue,
+        paymentStatus,
+        PurchaseOrderTransactions: {
+          delete: {
+            id: purchaseOrderTransactionId,
+          },
+        },
+      },
+    })
+
+    return success(res, { purchaseOrderData }, 'Purchase order transaction deleted successfully')
+  } catch (error) {
+    console.log(error)
+    return serverError(res, 'Failed to delete the purchase order transaction')
+  }
+}
+
 const purchaseOrderTransactionsRouter = Router()
 
 purchaseOrderTransactionsRouter.post('/', createPurchaseOrderTransaction)
+purchaseOrderTransactionsRouter.delete('/:purchaseOrderTransactionId', deletePurchaseOrderTransaction)
 
 export default purchaseOrderTransactionsRouter
